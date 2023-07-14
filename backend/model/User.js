99b7391fa21749a97764ba171793320f8d539a9b
Hitesh <hiteshbisht565@gmail.com>
2023-07-14 20:04:45 +0530

    initial commit

---
+++ backend/model/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please Enter Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Password"],
+    select: false,
+  },
+
+  timeline: [
+    {
+      title: String,
+      description: String,
+      date: Date,
+    },
+  ],
+
+  
+
+  projects: [
+    {
+      url: String,
+      title: String,
+      image: {
+        public_id: String,
+        url: String,
+      },
+      description: String,
+      techStack: String,
+    },
+  ],
+  about: {
+    name: String,
+    title: String,
+    subtitle: String,
+    description: String,
+    quote: String,
+ 
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
